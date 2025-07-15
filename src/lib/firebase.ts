3e07e87d7d3bc3bdfe94e7f8f067c739937df8c9
McRonaah <ronaldcheruiyot342@gmail.com>
2025-07-15 09:08:10 +0300

    :poop: setting up firebase configurations :lipstick:

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,24 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "/",
-  authDomain: "y/",
-  projectId: "/",
-  storageBucket: "/",
-  messagingSenderId: "1",
-  appId: "/"
+  appId: "1:422578595725:web:20e8318c1d0a0a8994ffc4",
+  apiKey: "AIzaSyCrkg8kUh2Pq1mWIifPrEAtSYeEW83Yl7M",
+  authDomain: "ask-manager.firebaseapp.com",
+  databaseURL: "https://ask-manager-default-rtdb.firebaseio.com",
+  projectId: "ask-manager",
+  storageBucket: "ask-manager.firebasestorage.app",
+  messagingSenderId: "422578595725",
+  measurementId: "G-CHGTBW42FY"
+
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // Initialize Firebase services
 export const auth = getAuth(app);
